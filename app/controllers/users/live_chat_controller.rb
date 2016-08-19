a5c0318f3c295ba6130e4f01257d67c276fde301
kaswan <kaswan@licenseacademy.jp>
2016-08-19 15:20:14 +0900

    update css for post

--- app/controllers/users/live_chat_controller.rb
+++ app/controllers/users/live_chat_controller.rb
@@ -9,9 +9,26 @@ class Users::LiveChatController < Users::ApplicationController
   def update
   end
   def connect
+    if params[:state] && !params[:room].blank?
+      @file = getChatfilePath(params[:room]);
+      if params[:state] == 'getState'
+        @log['state'] = getlines(getfile(@file));
+                       
+      end
+    end
     render layout: false if request.xhr?
   end
-#  def process
-#    render layout: false if request.xhr?
-#  end
+
+  private
+  
+  def getChatfilePath token
+    dir = Rails.root.join('public', 'chat')
+    Dir.mkdir(dir) unless Dir.exist?(dir)    
+  end
+  
+  def getlines    
+  end
+    
+  def getfile
+  end
 end
